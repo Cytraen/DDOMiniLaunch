9943e1f0766c0a842d4f6c7e2dbd8ff29c29edf5
ashakoor <60638768+ashakoor@users.noreply.github.com>
2021-06-19 04:13:21 -0400

    move update client to external implementation

--- MiniLaunch.WPFApp/App.xaml.cs
+++ MiniLaunch.WPFApp/App.xaml.cs
@@ -1,11 +1,8 @@
-﻿using AppUpdater.ClientLib;
-using AppUpdater.Common;
+﻿using CSharpDate.ClientLib;
 using MiniLaunch.Common;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.IO.Compression;
 using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -15,7 +12,7 @@ namespace MiniLaunch.WPFApp
 {
     public partial class App : Application
     {
-        public static ApiClient UpdateClient { get; set; }
+        public static CSharpDateClient UpdateClient { get; set; }
 
         public static SoapClient SoapClient { get; set; }
 
@@ -27,7 +24,7 @@ namespace MiniLaunch.WPFApp
 
         public App()
         {
-            UpdateClient = new();
+            UpdateClient = new("ddoml");
             SoapClient = new();
         }
 
@@ -57,99 +54,12 @@ namespace MiniLaunch.WPFApp
             mainWindow.Show();
         }
 
-        internal static async Task UpdateApp()
+        internal static Task UpdateApp()
         {
-            var info = await UpdateClient.GetProductInfo("ddoml");
-            var response = info.Response;
-            var currentVer = typeof(App).Assembly.GetName().Version;
-
-            var updateAvailable = IsUpdateAvailable(currentVer, response);
-
-            if (!updateAvailable)
-            {
-                return;
-            }
-
-            var result = MessageBox.Show("There is an update available.\nWould you like to update now?", "DDOMiniLaunch - Update Available", MessageBoxButton.YesNo);
-
-            if (result != MessageBoxResult.Yes)
-            {
-                return;
-            }
-
-            var downloadPath = Path.Combine(Config.DataFolder, "update.zip");
-            await UpdateClient.DownloadFile(response.Link, downloadPath);
-
-            var zipFile = ZipFile.OpenRead(downloadPath);
-            var dirs = zipFile.Entries.Where(x => x.Name == "" && x.Length == 0 && x.FullName.EndsWith('/')).ToList();
-
-            if (dirs.Count == 1 && zipFile.Entries.Where(x => x.FullName.StartsWith(dirs[0].FullName)).Count() == zipFile.Entries.Count)
-            {
-                ZipFile.ExtractToDirectory(downloadPath, Config.DataFolder);
-                Directory.Move(Path.Combine(Config.DataFolder, dirs[0].FullName), Path.Combine(Config.DataFolder, "new"));
-            }
-            else
-            {
-                ZipFile.ExtractToDirectory(downloadPath, Path.Combine(Config.DataFolder, "new"));
-            }
-
-            var files = Directory.GetFiles(Directory.GetCurrentDirectory());
-
-            foreach (var file in files)
-            {
-                var fileInfo = new FileInfo(file);
-                fileInfo.MoveTo(file + ".old.updated");
-            }
-
-            var newFiles = Directory.GetFiles(Path.Combine(Config.DataFolder, "new"));
-
-            foreach (var file in newFiles)
-            {
-                var fileInfo = new FileInfo(file);
-                var relativePath = file.Replace(Path.Combine(Config.DataFolder, "new"), "");
-
-                var recursiveDir = Directory.GetCurrentDirectory();
-
-                foreach (var dir in relativePath.Split('/')[..^1])
-                {
-                    recursiveDir = Path.Combine(recursiveDir, dir);
-                    var dirInfo = new DirectoryInfo(recursiveDir);
-                    if (!dirInfo.Exists)
-                    {
-                        Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), recursiveDir));
-                    }
-                }
-
-                fileInfo.MoveTo(Path.Combine(Directory.GetCurrentDirectory(), relativePath.TrimStart('/', '\\')));
-            }
-
-            //await Task.Delay(500);
-            //File.Delete(Path.Combine(Config.DataFolder, "update.zip"));
-            Directory.Delete(Path.Combine(Config.DataFolder, "new"), true);
-
-            var psi = new ProcessStartInfo();
-
-            psi.FileName = "cmd.exe";
-            psi.UseShellExecute = false;
-            psi.CreateNoWindow = true;
-            //psi.WorkingDirectory = Directory.GetCurrentDirectory();
-            psi.Arguments = "/C timeout /T 1 /NOBREAK && powershell.exe Del " + Directory.GetCurrentDirectory() + "\\* -include *.old.updated -recurse && powershell.exe Del " + Path.Combine(Config.DataFolder, "update.zip") + " && " + Directory.GetCurrentDirectory() + "\\DDOMiniLaunch.exe";
-
-            Process.Start(psi);
-            Environment.Exit(0);
+            return UpdateClient.CheckAndDownloadUpdate(typeof(App).Assembly.GetName().Version, AskToUpdate, default);
         }
 
-        private static bool IsUpdateAvailable(Version current, ProductInfo update)
-        {
-            var updateVer = new Version(update.Major, update.Minor, update.Patch);
-
-            if (updateVer > current)
-            {
-                return true;
-            }
-
-            return false;
-        }
+        private static bool AskToUpdate() => MessageBox.Show("There is an update available.\nWould you like to update now?", "DDOMiniLaunch - Update Available", MessageBoxButton.YesNo) == MessageBoxResult.Yes;
 
         private static async Task<List<ServerInfo>> GetServerInfo(bool preview = false)
         {
